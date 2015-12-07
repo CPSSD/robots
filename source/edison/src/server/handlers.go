81e0a1fe1a7fd6bb4fd78418493c7f1419004f22
JackSmith54 <jack.smith54@mail.dcu.ie>
2015-12-07 17:14:31 +0000

    maps/rdp integration

--- source/edison/src/server/handlers.go
+++ source/edison/src/server/handlers.go
@@ -24,13 +24,16 @@ func Index(w http.ResponseWriter, r *http.Request) {
 // sends json to web client
 func BitmapIndex(w http.ResponseWriter, r *http.Request){
 
-	RobotMap := maps.CreateMap()
-	RobotMap.DrawSquare(4)
+	//RobotMap := maps.RobotMap
+	//RobotMap := maps.CreateMap()
+	//RobotMap.DrawSquare(4)
+	
 
-	var bitmap [][]bool = RobotMap.GetBitmap()
+	bitmap, _ := maps.RobotMap.GetBitmap()
 	var jsonmap JsonMap = JsonMap{Code: "200", Message: "ok", Map: bitmap}
 
 	w.Header().Set("Content-Type","application/json;charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin","*")
 	w.WriteHeader(http.StatusOK)
 
 	if err := json.NewEncoder(w).Encode(jsonmap); err != nil {
