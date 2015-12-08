54dce6555f5192c6b0a7647af79ad4f45c727a29
DarraghGriffin <darragh.griffin24@mail.dcu.ie>
2015-12-08 14:33:39 +0000

    Directory structure and SPI wrapper layout

---
+++ source/arduino/SPI/SPI_Wrapper.cpp
@@ -0,0 +1,46 @@
+#include "SPI_Wrapper.h"
+
+#include <SPI.h>
+#include "Arduino.h"
+
+#define InitialTransferByte 100
+
+using namespace std;
+
+void SPI_Wrapper::init() 
+{
+    SPISettings(100000, MSBFIRST, SPI_MODE0);
+    SPI.begin();
+       
+    SPCR |= bit(SPE);
+    pinMode(MISO, OUTPUT);
+    SPI.attachInterrupt();
+
+	SPDR = InitialTransferByte;
+	
+    currentState = WaitingToBegin;
+}
+
+void SPI_Wrapper::registerResponseHandler(SPI_Response_Handler newResponseHandler)
+{
+	responseHandler = newResponseHandler;
+}
+
+ISR(SPI_STC_vect) 
+{
+    uint8_t byteReceived = SPDR;
+
+	switch (currentState) {
+		case WaitingToBegin: // If we receive 255 move to SendingLength state 
+							 // and put length of command to send in SPDR
+			break;
+		case SendingLength:  // byteReceived is the length of the command we are about to receive
+			break;
+		case SendingCommand: // if the number of bytes we have sent/receive >= sendingCommandLength and 
+							 // receivingCommandLength then we are done, process the command
+			break;
+		case CheckingIfFinished: // If we last sent 1 or byteReceived is 1 then we change state to sendingCommand. 
+							     // Else go to waiting
+			break;
+	}
+}
---
+++ source/arduino/SPI/SPI_Wrapper.h
@@ -0,0 +1,69 @@
+#ifndef SPI_WRAPPER_H
+#define SPI_WRAPPER_H
+
+#include "Arduino.h"
+#include <cstdint>
+
+#define MAX_BUFFER_SIZE 1024
+#define MAX_COMMAND_LENGTH 32
+
+using namespace std;
+
+typedef struct {
+	uint8_t commandNumber;
+	uint16_t uniqueID;
+} command;
+
+typedef struct moveCommand : command {
+	uint16_t angle;
+	uint16_t magnitude;
+} moveCommand;
+
+typedef struct stopCommand : command {
+	// No extra information for stop command
+} stopCommand;
+
+typedef struct rotateCommand : command {
+	uint16_t angle;
+} rotateCommand;
+
+typedef struct scanCommand : command {
+	// No extra information for scan command
+} scanCommand;
+
+typedef enum {
+	WaitingToBegin,
+	SendingLength,
+	SendingCommand,
+	CheckingIfFinished
+} SPI_state;
+
+typedef SPI_Response_Handler void (*responseHandler)(command);
+
+class SPI_Wrapper {
+    public:
+		static void init();
+		static void registerResponseHandler(SPI_Response_Handler newResponseHandler);
+		static void sendMoveResponse(uint16_t uniqueID, uint16_t magnitude, uint16_t angle, bool status);		
+		static void sendStopResponse(uint16_t uniqueID, uint16_t magnitude, uint16_t angle, bool status);
+		static void sendRotateResponse(uint16_t uniqueID, uint16_t angle, bool status);
+		static void sendScanResponse(uint16_t uniqueID, uint16_t angle, uint16_t magnitude, bool last, bool status);
+		
+    private:
+		static void ProcessReceivedCommand(int length); // Processes the last recieved command, creating the right struct and calls responseHandler
+		
+		static SPI_Response_Handler responseHandler;
+	
+		static uint8_t dataOutBuffer[MAX_BUFFER_SIZE]; // Circular buffer contatining each byte to send
+		static int bufferOutFillBegin;
+		static int bufferOutFillEnd;
+		static int sendingCommandLength;
+		
+        static SPI_state currentState;
+		
+		static uint8_t commandBuffer[MAX_COMMAND_LENGTH]
+		static int receivingCommandLength;
+		static int commandBytesReceived;        
+};
+
+#endif
