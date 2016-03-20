30209270302f8394d298543f95744c20a0a70f5c
Conor McManus <cgmc@users.noreply.github.com>
2016-03-20 00:19:06 +0000

    Remove debugging code

--- source/mega/simulator/libs/calc/calc.cpp
+++ source/mega/simulator/libs/calc/calc.cpp
@@ -26,42 +26,7 @@ struct MapLine calc::makeLineFromPolar(float angle, float distance, point curren
   if(yValueDelta >= -0.012f && yValueDelta <= 0.012f) {
 	  yValueDelta = 0.00;
   }
-  /*Serial.print("Cos(");
-  Serial.print(angle);
-  Serial.print(") = ");
-  Serial.println((cos(angle)));
-  Serial.print(distance);
-  Serial.print(" * Cos(");
-  Serial.print(angle);
-  Serial.print(") = ");
-  Serial.println(xValueDelta);
-  Serial.print(currentPosition.x);
-  Serial.print(" + (");
-  Serial.print(distance);
-  Serial.print(" * Cos(");
-  Serial.print(angle);
-  Serial.print(")) = ");
-  Serial.println(currentPosition.x + xValueDelta);
-  Serial.print("Sin(");
-  Serial.print(angle);
-  Serial.print(") = ");
-  Serial.println((sin(angle)));
-  Serial.print(distance);
-  Serial.print(" * Sin(");
-  Serial.print(angle);
-  Serial.print(") = ");
-  Serial.println(yValueDelta);
-  Serial.print(currentPosition.y);
-  Serial.print(" + (");
-  Serial.print(distance);
-  Serial.print(" * Sin(");
-  Serial.print(angle);
-  Serial.print(")) = ");
-  Serial.println(currentPosition.y + yValueDelta);*/
   temp.x2y2 = {(currentPosition.x + xValueDelta), (currentPosition.y + yValueDelta)};
-  /*Serial.print(temp.x2y2.x);
-  Serial.print(", ");
-  Serial.println(temp.x2y2.y);*/
   return temp;
 }
 
@@ -69,49 +34,14 @@ struct EquationOfLine calc::getEquationOfLine(MapLine line) {
   EquationOfLine equ;
   equ.xy.x = line.x1y1.x;
   equ.xy.y = line.x1y1.y;
-  /*Serial.print(line.x2y2.x);
-  Serial.print(" - ");
-  Serial.print(equ.xy.x);
-  Serial.print(" = ");
-  Serial.println(line.x2y2.x - equ.xy.x);*/
   if((line.x2y2.x - equ.xy.x) >= -0.01f && (line.x2y2.x - equ.xy.x) <= 0.01f) {
     equ.isVertical = true;
   }
   else {
     equ.isVertical = false;
   }
-  /*Serial.print("isVertical = ");
-  Serial.println(equ.isVertical);
-  Serial.print(line.x2y2.y);
-  Serial.print(" - ");
-  Serial.print(equ.xy.y);
-  Serial.print(" = ");
-  Serial.println(line.x2y2.y - equ.xy.y);
-  Serial.print(line.x2y2.x);
-  Serial.print(" - ");
-  Serial.print(equ.xy.x);
-  Serial.print(" = ");
-  Serial.println(line.x2y2.x - equ.xy.x);
-  Serial.print("(");
-  Serial.print(line.x2y2.y - equ.xy.y);
-  Serial.print(") / (");
-  Serial.print(line.x2y2.x - equ.xy.x);
-  Serial.print(") = ");
-  Serial.println((line.x2y2.y - equ.xy.y) / (line.x2y2.x - equ.xy.x));*/
   equ.m = (line.x2y2.y - equ.xy.y) / (line.x2y2.x - equ.xy.x); //Warning: this will be NaN if line is vertical (as denominator will be zero). The boolean allows for avoiding this posing a problem
-  /*Serial.print(equ.m);
-  Serial.print(" * ");
-  Serial.print(equ.xy.x);
-  Serial.print(" = ");
-  Serial.println(equ.m * equ.xy.x);
-  Serial.print("(-(");
-  Serial.print(equ.m * equ.xy.x);
-  Serial.print(")) + ");
-  Serial.print(equ.xy.y);
-  Serial.print(" = ");
-  Serial.println(-(equ.m * equ.xy.x) + equ.xy.y);*/
   equ.c = -(equ.m * equ.xy.x) + equ.xy.y;
-  //Serial.println(equ.c);
   return equ;
 }
 
@@ -132,50 +62,30 @@ struct point calc::getDestination(struct MapLine moveLine, struct EquationOfLine
   if(diffInXValuesDest >= -0.01f && diffInXValuesDest <= 0.01f) {
 	  diffInXValuesDest = 0.00;
   }
-  /*Serial.print("Sign of translation in x for destination: ");
-  Serial.println(diffInXValuesDest);
-  Serial.println(moveLine.x2y2.y);
-  Serial.println(moveLine.x1y1.y);
-  Serial.println(moveLine.x2y2.y - moveLine.x1y1.y);*/
   float diffInYValuesDest = moveLine.x2y2.y - moveLine.x1y1.y;
   if(diffInYValuesDest >= -0.01f && diffInYValuesDest <= 0.01f) {
 	  diffInYValuesDest = 0.00;
   }
-  //Serial.print("Sign of translation in y for destination: ");
-  //Serial.println(diffInYValuesDest);
   
   float diffInXValuesWall, diffInYValuesWall, distBetweenRobotAndDest, distBetweenRobotAndWall;
   //Get distance between robot and destination
   distBetweenRobotAndDest = getDistBetweenTwoPoints(robotLine.xy, nearestWall);
-  //Serial.print("Distance to dummy destination: ");
-  //Serial.println(distBetweenRobotAndDest);;
   
   for(int i = 0; i < indexVIP; i++) {
-	  /*Serial.print("Wall: (");
-	  Serial.print(validInterceptPoints[i].x);
-	  Serial.print(", ");
-	  Serial.print(validInterceptPoints[i].y);
-	  Serial.println(")");*/
 	//Determine sign of each translation for given interception point
 	diffInXValuesWall = validInterceptPoints[i].x - robotLine.xy.x;
 	if(diffInXValuesWall >= -0.01f && diffInXValuesWall <= 0.01f) {
 		diffInXValuesWall = 0.00;
 	}
-	//Serial.print("Sign of translation in x for wall: ");
-	//Serial.println(diffInXValuesWall);
 	diffInYValuesWall = validInterceptPoints[i].y - robotLine.xy.y;
 	if(diffInYValuesWall >= -0.01f && diffInYValuesWall <= 0.01f) {
 		diffInYValuesWall = 0.00;
 	}
-	//Serial.print("Sign of translation in y for wall: ");
-	//Serial.println(diffInYValuesWall);
 	//If the signs match, we're facing the right direction
 	if(( (diffInXValuesDest > 0.0 && diffInXValuesWall > 0.0) || (diffInXValuesDest == 0.0 && diffInXValuesWall == 0.0) || (diffInXValuesDest < 0.0 && diffInXValuesWall < 0.0) )
   &&( (diffInYValuesDest > 0.0 && diffInYValuesWall > 0.0) || (diffInYValuesDest == 0.0 && diffInYValuesWall == 0.0) || (diffInYValuesDest < 0.0 && diffInYValuesWall < 0.0) )) {
 	  //Get distance between robot and interception point
 	  distBetweenRobotAndWall = getDistBetweenTwoPoints(robotLine.xy, validInterceptPoints[i]);
-	  //Serial.print("Distance to wall: ");
-	  //Serial.println(distBetweenRobotAndWall);
 	  //If distance between robot and interception point is shorter than between robot and destination
 	  if(distBetweenRobotAndWall <= distBetweenRobotAndDest) {
 		  nearestWall = validInterceptPoints[i];
@@ -183,77 +93,32 @@ struct point calc::getDestination(struct MapLine moveLine, struct EquationOfLine
 		}
 	}
   }
-  /*Serial.print("Nearest wall: (");
-  Serial.print(nearestWall.x);
-  Serial.print(", ");
-  Serial.print(nearestWall.y);
-  Serial.println(")");*/
   return nearestWall;
 }
 
 boolean calc::hasInterception(EquationOfLine border, EquationOfLine robotMoveLine) {
-	/*Serial.print("Robot is vertical = ");
-	Serial.println(robotMoveLine.isVertical);
-	Serial.print("Wall is vertical = ");
-	Serial.println(border.isVertical);
-	Serial.print("Robot slope: ");
-	Serial.println(robotMoveLine.m);
-	Serial.print("Wall slope: ");
-	Serial.println(border.m);
-	Serial.print("Slopes equal?: ");
-	Serial.println(robotMoveLine.m == border.m);*/
 	if((robotMoveLine.isVertical && border.isVertical) || (robotMoveLine.m == border.m && (!robotMoveLine.isVertical || !border.isVertical))) { //Lines are parallel
-		//Serial.println("Lines are parallel.");
 		return false;
 	}
 	else {
 		//Line is in point-slope form (y = line.m(x - line.x) + line.y
 		//Convert to slope-intercept form: y = (line.m * x) + (line.m * line.x) + line.y
 		//y = line.m * x + ((line.m * line.x) + line.y)
-		/*Serial.print("Wall's x-coordinate: ");
-		Serial.println(border.xy.x);
-		Serial.print("Robot's y-coordinate: ");
-		Serial.println(robotMoveLine.xy.y);
-		Serial.print(robotMoveLine.m);
-		Serial.print(" * ");
-		Serial.print(border.xy.x);
-		Serial.print(" = ");
-		Serial.println(robotMoveLine.m * border.xy.x);
-		Serial.print(robotMoveLine.m * border.xy.x);
-		Serial.print(" + ");
-		Serial.print(robotMoveLine.xy.y);
-		Serial.print(" = ");
-		Serial.println((robotMoveLine.m * border.xy.x) + robotMoveLine.xy.y);
-		Serial.print("(");
-		Serial.print(robotMoveLine.m * border.xy.x);
-		Serial.print(") + ((");
-		Serial.print(robotMoveLine.m * border.xy.x);
-		Serial.print(") + ");
-		Serial.print(robotMoveLine.xy.y);
-		Serial.print(") = ");
-		Serial.println((robotMoveLine.m * border.xy.x) + ((-(robotMoveLine.m * robotMoveLine.xy.x)) + robotMoveLine.xy.y));*/
 		float intercept = ((robotMoveLine.m * border.xy.x) + ((-(robotMoveLine.m * robotMoveLine.xy.x)) + robotMoveLine.xy.y));
-		//Serial.print("Intercept: ");
-		//Serial.println(intercept);
 		if(robotMoveLine.isVertical || border.isVertical) {
 			if(robotMoveLine.isVertical) {
-				//Serial.println("Since wall is not vertical, the robot's line must intersect the wall somewhere (if not necessarily at a sane location).");
 				return true;
 			}
 			if(border.isVertical) {
-				//Serial.print("Wall is vertical, and the robot's line is not, ");
 				if(intercept >= 0.0 && intercept <= 300.0) {
-					//Serial.println("and the intercept point is in a sane space.");
 					return true;
 				}
 				else {
-					//Serial.println("but the intercept point is out of bounds.");
 					return false;
 				}
 			}
 		}
 		else {
-			//Serial.println("Neither line is vertical.");
 			return true;
 		}
 	}
@@ -275,53 +140,14 @@ struct point calc::getInterceptPoint(EquationOfLine robotLine, EquationOfLine ot
   else {
     //We set the two equations equal to each other and manipulate the expression to solve for x.
     // m1x + c1 = m2x + c2 -> m1x - m2x = c2 - c1 -> x = (c2 - c1) / (m1 - m2)
-	/*Serial.print("Robot slope: ");
-	Serial.println(robotLine.m);
-	Serial.print("Wall slope: ");
-	Serial.println(other.m);
-	Serial.print(robotLine.m);
-	Serial.print(" - ");
-	Serial.print(other.m);
-	Serial.print(" = ");
-	Serial.println(robotLine.m - other.m);*/
     float denominator = robotLine.m - other.m;
-	/*Serial.print("Wall's c: ");
-	Serial.println(other.c);
-	Serial.print("Robot's c: ");
-	Serial.println(robotLine.c);
-	Serial.print(other.c);
-	Serial.print(" - ");
-	Serial.print(robotLine.c);
-	Serial.print(" = ");
-	Serial.println(other.c - robotLine.c);*/
     float numerator = other.c - robotLine.c;
-	/*Serial.print(numerator);
-	Serial.print(" / ");
-	Serial.print(denominator);
-	Serial.print(" = ");
-	Serial.println(numerator / denominator);*/
     intercept.x = numerator / denominator;
     //Substitute the x-value into either equation of the lines and solve for y
-	/*Serial.print(robotLine.m);
-	Serial.print(" * ");
-	Serial.print(intercept.x);
-	Serial.print(" = ");
-	Serial.println(robotLine.m * intercept.x);
-	Serial.print("(");
-	Serial.print(robotLine.m * intercept.x);
-	Serial.print(") + ");
-	Serial.print(robotLine.c);
-	Serial.print(" = ");
-	Serial.println((robotLine.m * intercept.x) + robotLine.c);*/
     intercept.y = (robotLine.m * intercept.x) + robotLine.c;
 	if(intercept.y >= -0.01f && intercept.y <= 0.01f) {
 		intercept.y = 0.00;
 	}
   }
-  /*Serial.print("Intercept point: (");
-  Serial.print(intercept.x);
-  Serial.print(", ");
-  Serial.print(intercept.y);
-  Serial.println(")");*/
   return intercept;
 }
