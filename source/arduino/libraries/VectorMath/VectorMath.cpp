70a233efcda77083dcbd296c58a246d91731bba3
JackSmith54 <jack.smith54@mail.dcu.ie>
2016-04-11 11:42:58 +0100

    ensure wheels stop at same time

--- source/arduino/libraries/VectorMath/VectorMath.cpp
+++ source/arduino/libraries/VectorMath/VectorMath.cpp
@@ -35,6 +35,79 @@ bool VectorMath::setSign(int angle){
     return positive;
 }
 
+double VectorMath::ticksPercentage(int angle, int distance){
+    if(FLBR_Wheels){
+        Serial.print("Angle: ");
+        Serial.println(FL_Ticks(angle));
+        Serial.print("Distance: ");
+        Serial.println(distance);
+        return (distance / 90.00) * FL_Ticks(angle); // 90.00;
+    }else{
+        Serial.print("Here: ");
+        Serial.println(FR_Ticks(angle));
+        double temp = distance / 90.00;
+        temp = temp * FR_Ticks(angle);
+        return temp;
+    }
+}
+
+
+int VectorMath::FL_Ticks(int angle){
+    if(angle > 315 || angle < 135){
+        return FL_Forward(angle);
+    }else{
+        return FL_Backward(angle);
+    }
+}
+
+int VectorMath::FL_Forward(int angle){
+    if(angle > 315){
+        angle = angle - 315;
+    }else if(angle < 45){
+        angle = angle + 45;
+    }else if(angle > 44 && angle < 135){
+        angle = 135 - angle;
+    }
+    return angle;
+}
+
+int VectorMath::FL_Backward(int angle){
+    if(angle > 134 && angle < 225){
+        angle = angle - 135;
+        //Serial.println("Made it");
+    }else{
+        angle = 315 - angle;
+    }
+    return angle;
+}
+
+int VectorMath::FR_Ticks(int angle){
+    if(angle < 45 || angle > 225){
+        return FR_Forward(angle);
+    }else{
+        return FR_Backward(angle);
+    }
+}
+
+int VectorMath::FR_Forward(int angle){
+    if(angle < 45){
+        angle = abs(angle - 45);
+    }else if(angle > 314){
+        angle = 405 - angle;
+    }else if(angle > 225 && angle < 315){
+        angle = angle - 225;
+    }
+    return angle;
+}
+
+int VectorMath::FR_Backward(int angle){
+    if(angle < 226 && angle > 135){
+        return abs(angle - 225);
+    }else{
+        return angle - 45;
+    }
+}
+
 bool VectorMath::getSign(){
   return positive;
 }
--- source/arduino/libraries/VectorMath/VectorMath.h
+++ source/arduino/libraries/VectorMath/VectorMath.h
@@ -11,7 +11,14 @@ class VectorMath{
         static double degreesToRadians(double angle);
         static double radiansToDegrees(double angle);
         static double computeSpeed(int angle);
+        static double ticksPercentage(int angle, int distance);
     private:
+        static int FL_Ticks(int angle);
+        static int FL_Forward(int angle);
+        static int FL_Backward(int angle);
+        static int FR_Ticks(int angle);
+        static int FR_Forward(int angle);
+        static int FR_Backward(int angle);
         static bool positive;
         static bool FLBR_Wheels;
         static moveCommand currentMove;
