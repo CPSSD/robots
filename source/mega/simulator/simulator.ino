6566e180ff61d3d54b3a421673a640878bb4a13b
Conor McManus <cgmc@users.noreply.github.com>
2016-04-16 22:38:50 +0100

    Add rotation to simulator
    As the angle contained in a rotateCommand would be an unsigned integer,
    it was assumed that the angle should be taken as an absolute value.
    
    References #194.

--- source/mega/simulator/simulator.ino
+++ source/mega/simulator/simulator.ino
@@ -37,8 +37,9 @@ int angleSlip = 3;
 void setup() {
   SPI_Wrapper::init();
   SPI_Wrapper::registerMoveCommandHandler(&moveCommandHandler);
-  SPI_Wrapper::registerScanCommandHandler(&scanCommandHandler);
   SPI_Wrapper::registerStopCommandHandler(&stopCommandHandler);
+  SPI_Wrapper::registerRotateCommandHandler(&rotateCommandHandler);
+  SPI_Wrapper::registerScanCommandHandler(&scanCommandHandler);
   Serial.begin(9600);
   Serial.println("Starting...");
   currentPosition.x = STARTING_X;
@@ -50,6 +51,7 @@ void setup() {
   Serial.print("Adding object 1: ");
   Serial.println(room.addObject(0, 4, newObjHolder));
   scanTimer = millis();
+  rotateTimer = millis();
 }
 
 void loop() {
@@ -112,6 +114,12 @@ void stopCommandHandler(stopCommand stopCom) {
   }
 }
 
+void rotateCommandHandler(rotateCommand rotCom) {
+  if (com == NULL) {
+    com =  new rotateCommand(rotCom);
+  }
+}
+
 void scanCommandHandler(scanCommand scanCom) {
   Serial.println("Recieved Scan Command...");
   if (com == NULL) {
@@ -133,8 +141,12 @@ void processCommand(command* com) {
     
     SPI_Wrapper::sendStopResponse(com->uniqueID, (uint16_t)distanceMoved, (uint16_t)movingAngle, true);
   }
-  else if(com->commandNumber == rotateNum){
-    // rotate command to be implemented
+  else if(com->commandNumber == rotateNum) {
+    com->uniqueID = uniqueID;
+    uniqueID++;
+    respond((rotateCommand*)com);
+    delete com;
+    com = NULL;
   }
   else if(com->commandNumber == scanNum){
     amScanning = true;
@@ -144,6 +156,11 @@ void processCommand(command* com) {
 
 void respond(moveCommand* com){
   SPI_Wrapper::sendMoveResponse(com->uniqueID, distTravelled, movingAngle, true);
+}
+
+void respond(rotateCommand* com) {
+  physicalAngle = ((com->angle + 90) % 360);
+  SPI_Wrapper::sendRotateResponse(com->uniqueID, com->angle, true);
 }
 
 void respond(scanResponse scanResp) {
